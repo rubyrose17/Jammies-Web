ea4a34aa433d3d0a9255aad74f7fdf1de8d3a1a2
cesarpizarra <cpizarra002@gmail.com>
2023-11-06 15:33:29 +0800

    add 404

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,20 @@ mongoose.connection.once("open", () => {
 
 //Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://jammies-web.vercel.app/",
+  })
+);
 
 //Routes
 app.use("/api", userRoute);
 const port = process.env.PORT || 8080;
+
+//message 404
+app.use((req, res, next) => {
+  res.status(404).send("Page not Found!");
+});
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`.underline.cyan);
